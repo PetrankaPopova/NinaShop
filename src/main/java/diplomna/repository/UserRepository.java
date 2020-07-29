0469a929ddbf2b37afa1c22fd606feced6c28435
Petranka Popova <petranka_popova_@abv.bg>
2020-07-29 13:15:22 +0300

    .

--- src/main/java/diplomna/repository/UserRepository.java
+++ src/main/java/diplomna/repository/UserRepository.java
@@ -18,4 +18,8 @@ public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findUserByUsernameAndPassword(String username, String password);
 
     Optional<User> findUserByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username,String email);
 }
---
+++ src/main/java/diplomna/view/UserEditViewModel.java
@@ -0,0 +1,56 @@
+package diplomna.view;
+
+
+public class UserEditViewModel {
+
+    private String username;
+    private String password;
+    private String oldPassword;
+    private String confirmPassword;
+    private String email;
+
+
+    public UserEditViewModel() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+}
