891accf7b05779961ec650704b64bc0e92d6185d
Petranka Popova <petranka_popova_@abv.bg>
2020-08-03 06:53:13 +0300

    .

--- src/main/java/diplomna/web/controllers/SizeController.java
+++ src/main/java/diplomna/web/controllers/SizeController.java
@@ -1,15 +1,4 @@
 package diplomna.web.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/size")
 public class SizeController {
-
-    @GetMapping("")
-    public String showSize(){
-        return "size";
-    }
 }
